91738a18414d72a9534ba54ffb9468a9bc487b67
mattcs2401 <mattcs2401@gmail.com>
2024-01-16 16:07:19 +1000

    Comment out AppPreferences, not used ATT.

--- app/src/main/java/com/mcssoft/raceday/data/repository/preferences/app/AppPreferences.kt
+++ app/src/main/java/com/mcssoft/raceday/data/repository/preferences/app/AppPreferences.kt
@@ -1,9 +1,8 @@
 package com.mcssoft.raceday.data.repository.preferences.app
 
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class AppPreferences(
-    val meetingId: Long = 0,
-    val raceId: Long = 0
-)
+/** TBA **/
+//@Serializable
+//data class AppPreferences(
+//    val meetingId: Long = 0,
+//    val raceId: Long = 0
+//)
--- app/src/main/java/com/mcssoft/raceday/data/repository/preferences/app/AppPrefsSerializer.kt
+++ app/src/main/java/com/mcssoft/raceday/data/repository/preferences/app/AppPrefsSerializer.kt
@@ -1,38 +1,31 @@
 package com.mcssoft.raceday.data.repository.preferences.app
 
-import androidx.datastore.core.Serializer
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import kotlinx.serialization.SerializationException
-import kotlinx.serialization.json.Json
-import java.io.InputStream
-import java.io.OutputStream
-
-object AppPrefsSerializer : Serializer<AppPreferences> {
-
-    override val defaultValue: AppPreferences
-        get() = AppPreferences()
-
-    override suspend fun readFrom(input: InputStream): AppPreferences {
-        return try {
-            Json.decodeFromString(
-                deserializer = AppPreferences.serializer(),
-                string = input.readBytes().decodeToString()
-            )
-        } catch (e: SerializationException) {
-            e.printStackTrace()
-            defaultValue
-        }
-    }
-
-    override suspend fun writeTo(t: AppPreferences, output: OutputStream) {
-        withContext(Dispatchers.IO) {
-            output.write(
-                Json.encodeToString(
-                    serializer = AppPreferences.serializer(),
-                    value = t
-                ).encodeToByteArray()
-            )
-        }
-    }
-}
+/** TBA **/
+//object AppPrefsSerializer : Serializer<AppPreferences> {
+//
+//    override val defaultValue: AppPreferences
+//        get() = AppPreferences()
+//
+//    override suspend fun readFrom(input: InputStream): AppPreferences {
+//        return try {
+//            Json.decodeFromString(
+//                deserializer = AppPreferences.serializer(),
+//                string = input.readBytes().decodeToString()
+//            )
+//        } catch (e: SerializationException) {
+//            e.printStackTrace()
+//            defaultValue
+//        }
+//    }
+//
+//    override suspend fun writeTo(t: AppPreferences, output: OutputStream) {
+//        withContext(Dispatchers.IO) {
+//            output.write(
+//                Json.encodeToString(
+//                    serializer = AppPreferences.serializer(),
+//                    value = t
+//                ).encodeToByteArray()
+//            )
+//        }
+//    }
+//}
